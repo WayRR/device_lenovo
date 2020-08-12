289aa20858d18cc41f041117f74911ce03c340c5
WayRR <WayRROne@gmail.com>
2020-08-12 11:48:37 +0300

    Lunch Choisec

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_heart.mk
+    $(LOCAL_DIR)/_heart.mk
+
+COMMON_LUNCH_CHOICES := \
+    _heart-user \
+    _heart-userdebug \
+    _heart-eng
